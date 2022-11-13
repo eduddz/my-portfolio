ba7bd5bc76e2afe02fe1d2cc141aa06f5af0f3b4
José Eduardo Dorta Esteves <esteves-dorta@hotmail.com>
2022-11-13 19:09:40 -0300

    Estruturando o Projeto

---
+++ README.md
@@ -0,0 +1,35 @@
+# Portfolio 🏆
+
+Este website para mostrar meus projects em só lugar.
+Aqui vou mostrar todos os projetos que já desenvolvi.
+Também vou mostrar sobre mim e minhas exeperiências.
+As tecnologias foram escolhidas a dedo e estou usando:
+    html
+    css
+    javascript
+    react
+    typescript
+    sass
+
+## Code 👨🏻‍💻
+
+    npm install vite@latest
+    |
+    project name: portfolio
+    |
+    cd portfolio
+    |
+    npm install
+
+For install sass
+
+    npm i sass
+
+## Explaining a little about the development of the project
+
+Tive a ideia de criar um porfolio novo, focado no visual (🎨) sem perder a responsividade.
+Não sabia muito por onde ir, por isso, acabei procurando algumas referências no Dribbble, Pinterest e o Youtube, e nenhuma me agradou. Então resolvi fazer do meu jeito: "Criei conforme fui desenvolvendo".
+
+![Capture 1](./src/assets/capture/Capture1.png)
+Esse é o primeiro esboço (📝). Jà está estruturado, mostrando onde todos os componentes ficarão e agora é só lapidar esse incrível diamante (💎). 
+Considero esse o jeito mais produtivo, pois consigo definir onde tão os componentes e o código mais performático para tal.
---
+++ src/App.tsx
@@ -0,0 +1,184 @@
+import "./style.scss";
+
+export function App() {
+  return (
+    <>
+      <header>
+        <div>
+          <p>img</p>
+          <nav>
+            <ul>
+              <li>home</li>
+              <li>projects</li>
+              <li>contact</li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <main>
+        <section className="first">
+          <div>
+            <h1>Eduardo Esteves</h1>
+            <p>Sou desenvolvedor FULLSTACK! Estou em constante evolução! Aproveita que está aqui no site, para conferir meus projetos {":)"}</p>
+            <p>Para me seguir ou chamar para conversar:</p>
+            <div className="social">
+              <p>img github</p>
+              <p>img linkdin</p>
+              <p>img youtube</p>
+              <p>img instagram</p>
+            </div>
+          </div>
+          <p>img</p>
+        </section>
+        <section className="second">
+          <h2>Minhas habilidades</h2>
+          <div>
+            <div>
+              <p>img</p>
+              <p>react</p>
+              <p>5 estrelas</p>
+            </div>
+            <div>
+              <p>img</p>
+              <p>react</p>
+              <p>5 estrelas</p>
+            </div>
+            <div>
+              <p>img</p>
+              <p>react</p>
+              <p>5 estrelas</p>
+            </div>
+            <div>
+              <p>img</p>
+              <p>react</p>
+              <p>5 estrelas</p>
+            </div>
+            <div>
+              <p>img</p>
+              <p>react</p>
+              <p>5 estrelas</p>
+            </div>
+            <div>
+              <p>img</p>
+              <p>react</p>
+              <p>5 estrelas</p>
+            </div>
+            <div>
+              <p>img</p>
+              <p>react</p>
+              <p>5 estrelas</p>
+            </div>
+            <div>
+              <p>img</p>
+              <p>react</p>
+              <p>5 estrelas</p>
+            </div>
+          </div>
+        </section>
+        <section className="third">
+          <h2>Projects</h2>
+          <div>
+            <div>
+              <p>img</p>
+              <p>Project #1</p>
+            </div>
+            <div>
+              <p>img</p>
+              <p>Project #1</p>
+            </div>
+            <div>
+              <p>img</p>
+              <p>Project #1</p>
+            </div>
+            <div>
+              <p>img</p>
+              <p>Project #1</p>
+            </div>
+            <div>
+              <p>img</p>
+              <p>Project #1</p>
+            </div>
+            <div>
+              <p>img</p>
+              <p>Project #1</p>
+            </div>
+            <div>
+              <p>img</p>
+              <p>Project #1</p>
+            </div>
+            <div>
+              <p>img</p>
+              <p>Project #1</p>
+            </div>
+            <div>
+              <p>img</p>
+              <p>Project #1</p>
+            </div>
+            <div>
+              <p>img</p>
+              <p>Project #1</p>
+            </div>
+            <div>
+              <p>img</p>
+              <p>Project #1</p>
+            </div>
+            <div>
+              <p>img</p>
+              <p>Project #1</p>
+            </div>
+            <div>
+              <p>img</p>
+              <p>Project #1</p>
+            </div>
+            <div>
+              <p>img</p>
+              <p>Project #1</p>
+            </div>
+            <div>
+              <p>img</p>
+              <p>Project #1</p>
+            </div>
+            <div>
+              <p>img</p>
+              <p>Project #1</p>
+            </div>
+            <div>
+              <p>img</p>
+              <p>Project #1</p>
+            </div>
+            <div>
+              <p>img</p>
+              <p>Project #1</p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <h2>Contact</h2>
+          <form>
+            <div>
+              <label>
+                <input placeholder="name" />
+              </label>
+              <label>
+                <input placeholder="e-mail" />
+              </label>
+            </div>
+            <div>
+              <label>
+                <textarea />
+              </label>
+            </div>
+            <div>
+              <button>Send</button>
+              <button>Reset</button>
+            </div>
+          </form>
+        </section>
+      </main>
+      <footer>
+        <h2>Eduardo Esteves</h2>
+        <p>@developer - 2023</p>
+      </footer>
+    </>
+  )
+}
